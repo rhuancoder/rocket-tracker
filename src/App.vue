fc82ff68ef86898321ade4acdc7684b27917109e
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2023-09-23 10:46:13 -0300

    feat: add form panel

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,23 @@
       <SideBar />
     </div>
     <div class="column is-three-quarter">
-      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      <FormPanel />
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import SideBar from './components/SideBar.vue';
+import { defineComponent } from 'vue'
+import SideBar from './components/SideBar.vue'
+import FormPanel from './components/FormPanel.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
-    SideBar
+    SideBar,
+    FormPanel
   }
-});
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,26 +1,25 @@
 <template>
-    <header>
-        <h1>
-            <img src="../assets/logo.png" alt="">
-        </h1>
-    </header>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
-export default defineComponent({
-    name: 'SideBar'
-});
+export default defineComponent({ name: "SideBar" });
 </script>
 
-<style scoped>
+<style scoped="scoped">
 header {
   padding: 1rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
 }
+
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
