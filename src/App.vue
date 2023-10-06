bdad6f7eb219c0755cce9e6d2f3f64fec50a0282
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2023-10-05 21:22:14 -0300

    feat: add task list

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
     </div>
     <div class="column is-three-quarter">
       <FormPanel />
+      <div class="tasks">
+        <TaskModel />
+        <TaskModel />
+        <TaskModel />
+        <TaskModel />
+      </div>
     </div>
   </main>
 </template>
@@ -13,14 +19,20 @@
 import { defineComponent } from 'vue'
 import SideBar from './components/SideBar.vue'
 import FormPanel from './components/FormPanel.vue'
+import TaskModel from './components/TaskModel.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     SideBar,
-    FormPanel
+    FormPanel,
+    TaskModel
   }
 })
 </script>
 
-<style></style>
+<style>
+.tasks {
+ padding: 1.25rem;
+}
+</style>
