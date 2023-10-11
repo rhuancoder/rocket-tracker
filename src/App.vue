5c5535d084d7beec3aba70c3ccca76af9b130a40
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2023-10-11 14:59:24 -0300

    refactor: update task list

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,9 @@
       <SideBar />
     </div>
     <div class="column is-three-quarter">
-      <FormPanel />
+      <FormPanel @onSaveTask="saveTask" />
       <div class="tasks">
-        <TaskModel />
-        <TaskModel />
-        <TaskModel />
-        <TaskModel />
+        <TaskModel v-for="(task, index) in tasks" :key="index" :task="task"/>
       </div>
     </div>
   </main>
@@ -20,6 +17,7 @@ import { defineComponent } from 'vue'
 import SideBar from './components/SideBar.vue'
 import FormPanel from './components/FormPanel.vue'
 import TaskModel from './components/TaskModel.vue'
+import ITask from './interfaces/ITask'
 
 export default defineComponent({
   name: 'App',
@@ -27,12 +25,22 @@ export default defineComponent({
     SideBar,
     FormPanel,
     TaskModel
+  },
+  data() {
+    return {
+      tasks: [] as ITask[]
+    }
+  },
+  methods: {
+    saveTask(task: ITask) {
+      this.tasks.push(task)
+    }
   }
 })
 </script>
 
 <style>
 .tasks {
- padding: 1.25rem;
+  padding: 1.25rem;
 }
 </style>
--- src/components/TaskModel.vue
+++ src/components/TaskModel.vue
@@ -2,22 +2,29 @@
   <div class="box has-text-weight-bold">
     <div class="columns">
       <div class="column is-7">
-        Task description
+        {{ task.description }}
       </div>
       <div class="column">
-        <TimerViewer :timeInSeconds="15"/>
+        <TimerViewer :timeInSeconds="task.timeInSeconds" />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import TimerViewer from './TimerViewer.vue'
+import ITask from '../interfaces/ITask';
 
 export default defineComponent({
   name: 'TaskModel',
-  components: { TimerViewer }
+  components: { TimerViewer },
+  props: {
+    task: {
+      type: Object as PropType<ITask>,
+      required: true
+    }
+  }
 })
 </script>
 
